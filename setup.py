4f0ef5972688e7d3a8335a246809d5a3c5fb55ab
unbeatable2718 <152500308+unbeatable2718@users.noreply.github.com>
2024-04-07 18:10:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,20 +4,21 @@
 # For details on the licensing terms, see the LICENSE file.
 # SPDX-License-Identifier: MIT
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name="PFDL Scheduler",
+    name="pfdl_scheduler",
     version="0.9.1",
     description="Execution engine for Production Flow Description Language (PFDL) files.",
     author="Peter Detzner, Maximilian Hoerstrup",
     author_email="maximilian.hoerstrup@iml.fraunhofer.de",
+    packages=find_packages(),
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3 :: Only",
     ],
     install_requires=[
-        "antlr4-python3-runtime==4.9.3",
+        "antlr4-python3-runtime>=4.9.3",
         "antlr-denter",
         "snakes",
     ],  # external packages as dependencies
